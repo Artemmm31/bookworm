9df3f341a1cdd505e7994425ce31d36e6c45878e
name <you@example.com>
2025-11-06 14:18:26 +0300

    server part

--- server/src/routes/bookRoutes.ts
+++ server/src/routes/bookRoutes.ts
@@ -29,6 +29,51 @@ router.post('/', protectRoute, async (req, res) => {
         const err = error as Error;
         res.status(500).json({message: err.message})
     }
+});
+
+router.get('/', protectRoute, async (req, res) => {
+    try {
+        const page = Number(req.query.page) || 1;
+        const limit = Number(req.query.limit) || 5;
+        const skip = (page - 1) * limit;
+
+
+        const books = await Book.find().sort({ createdAt: -1}).skip(skip).limit(limit).populate("user", 'username profileImage');
+        const totalBooks = await Book.countDocuments();
+        res.send({
+            books,
+            currentPage: page,
+            totalBooks,
+            totalPages: Math.ceil(totalBooks / limit),
+        });
+    } catch (error) {
+        console.log('error creating a book', error);
+        const err = error as Error;
+        res.status(500).json({message: err.message})
+    }
+})
+
+router.delete('/:id', protectRoute, async (req, res) => {
+    try {
+        const book = await Book.findById(req.params.id);
+        if(!book) return res.status(404).json({message: 'Book not found'});
+        if(book.user.toString() !== (req as any).user._id.toString()) return res.status(401).json({message: 'Unauthorized'});
+        
+        if(book.image && book.image.includes('cloudinary')) {
+            try {
+                const publicId = book.image.split('/').pop()?.split('.')[0];
+                await cloudinary.uploader.destroy(publicId!);
+            } catch (error) {
+                console.log("error deleting image from cloudinary", error);
+            }
+        }
+
+        await book.deleteOne()
+        res.json({message: 'Book deleted successfully'});
+    } catch (error) {
+        console.log("Error deleting book", error);
+        res.status(500).json({message: 'Internal server error'});
+    }
 })
 
 export default router;
